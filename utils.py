890699fb7968421bee5c0764520333cd6de68bd4
Thomas Dürr <thomduerr@gmail.com>
2022-05-23 19:30:02 +0200

    remove widget

--- utils.py
+++ utils.py
@@ -1,9 +1,26 @@
 import numpy as np
 from pathlib import Path
 from PIL import Image
+import ipywidgets as widgets
+from IPython.display import display
 import cv2
 import dlib
 import pickle
+
+
+def remove_image(btn):
+    print(f'removed image {btn.image_path}')
+    btn.style.button_color = 'LightCoral'
+    btn.description = 'removed'
+    Path(btn.image_path).unlink()
+
+
+def remove_button(file_path):
+    button = widgets.Button(description='remove', button_style='')
+    button.on_click(remove_image)
+    button.image_path = file_path
+    return button
+
 
 def print_images(source_folder='cartoon-images', target_folder='cartoon-images-aligned', image_filter='aligned', landmarks=False, size=256):
 
@@ -34,7 +51,9 @@ def print_images(source_folder='cartoon-images', target_folder='cartoon-images-a
             img2 = np.array(Image.new('RGB', (size, size), (255, 255, 255)))
 
         if image_filter == 'aligned' and aligned_image.exists():
-            print(f'\n{image_path}:')
+            # print(f'\n{image_path}:')
+            btn = remove_button(str(aligned_image))
+            display(widgets.HBox([ widgets.Label(value=image_path), btn], layout=widgets.Layout(margin='30px 0 0 0')))
             if landmarks:
                 p = img1.shape[0] - img2.shape[0]
                 img2 = np.pad(img2, pad_width=((0, p), (0, 0), (0, 0)), mode='constant', constant_values=255)
